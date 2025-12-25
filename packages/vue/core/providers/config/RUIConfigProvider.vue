09f91ab2da0bd8c4c2f68a94e540b2d356e47e80
Hwacc <mshcccch@gmail.com>
2025-12-25 18:36:40 +0800

    feat: add RUIConfigProvider and configuration composables
    - Introduced `RUIConfigProvider` component for managing application-wide configuration.
    - Added `useConfig` composable to facilitate configuration retrieval and merging.
    - Updated `package.json` files to include new dependencies for `@iconify/vue` and `vue-component-type-helpers`.
    - Enhanced `pnpm-lock.yaml` with new package resolutions and dependencies.
    - Created new TypeScript interfaces for configuration context management.

---
+++ packages/vue/core/providers/config/RUIConfigProvider.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { RUIConfigContext } from './rui-config-context'
+import { computed } from 'vue'
+import { provideRUIConfigContext } from './rui-config-context'
+
+const props = defineProps<{
+  value?: RUIConfigContext
+}>()
+
+provideRUIConfigContext(computed(() => props.value ?? {}))
+</script>
+
+<template>
+  <slot />
+</template>
---
+++ packages/vue/core/providers/config/rui-config-context.ts
@@ -0,0 +1,26 @@
+import type {
+  IconifyIcon,
+  IconifyJSON,
+  PartialIconifyAPIConfig,
+} from '@iconify/vue'
+import type { ComputedRef } from 'vue'
+import type { ThemeContext } from '../theme/theme-context'
+import { createContext } from '@ark-ui/vue'
+
+export interface RUIConfigContext {
+  theme?: ThemeContext
+  tooltip?: {
+    closeDelay?: number
+    openDelay?: number
+    lazyMount?: boolean
+    unmountOnExit?: boolean
+  }
+  iconify?: {
+    addIcons?: Array<[string, IconifyIcon | null]>
+    addCollections?: Array<[IconifyJSON, string | undefined]>
+    addAPIProviders?: Array<[string, PartialIconifyAPIConfig]>
+  }
+}
+
+export const [provideRUIConfigContext, injectRUIConfigContext]
+  = createContext<ComputedRef<RUIConfigContext>>('RUIConfigContext')
