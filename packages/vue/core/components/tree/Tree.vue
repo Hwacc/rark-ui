9c2eb31935898b819890432eaa1aa2ccc977ac39
Hwacc <mshcccch@gmail.com>
2026-02-02 01:48:59 +0800

    feat: add TreeView component and integrate @zag-js/tree-view
    - Introduced TreeView and TreeNode components for hierarchical data representation.
    - Added TypeScript definitions for TreeView events and props.
    - Integrated @zag-js/tree-view for enhanced functionality and event handling.
    - Updated package dependencies to include @zag-js/tree-view version 1.33.1.

--- packages/vue/core/components/tree/Tree.vue
+++ packages/vue/core/components/tree/Tree.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-export interface TreeViewProps<T> extends TreeViewRootBaseProps<T>, Theme {
+export interface TreeProps<T> extends TreeViewRootBaseProps<T>, Theme {
   class?: HTMLAttributes['class']
 }
 </script>
@@ -8,21 +8,25 @@ export interface TreeViewProps<T> extends TreeViewRootBaseProps<T>, Theme {
 import type { TreeNode, TreeViewRootBaseProps } from '@ark-ui/vue/tree-view'
 import type { Theme } from '@rui-ark/vue-core/providers/theme'
 import type { HTMLAttributes } from 'vue'
-import type { TreeViewRootEmits } from './tree-view.types'
+import type { TreeViewRootEmits } from './tree'
 import { useForwardProps } from '@ark-ui/vue'
 import { TreeView, useTreeView } from '@ark-ui/vue/tree-view'
 import { ThemeProvider } from '@rui-ark/vue-core/providers/theme'
 
-const { theme, ...props } = defineProps<TreeViewProps<T>>()
+const { theme, ...props } = defineProps<TreeProps<T>>()
 const emits = defineEmits<TreeViewRootEmits<T>>()
-const forwarded = useForwardProps<TreeViewProps<T>, any>(props)
+const forwarded = useForwardProps<TreeProps<T>, any>(props)
 const treeView = useTreeView<T>(forwarded, emits)
 </script>
 
 <template>
   <TreeView.RootProvider :value="treeView">
     <ThemeProvider>
-      <slot />
+      <slot name="prefix" />
+      <TreeView.Tree>
+        <slot />
+      </TreeView.Tree>
+      <slot name="suffix" />
     </ThemeProvider>
   </TreeView.RootProvider>
 </template>
---
+++ packages/vue/core/components/tree/index.ts
@@ -0,0 +1,1 @@
+export { default as Tree, type TreeProps } from './Tree.vue'
