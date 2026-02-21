62c3401808b6b1f7662cdc492b939317c0bcabf1
Hwacc <mshcccch@gmail.com>
2026-02-21 23:28:09 +0800

    feat: enhance Tailwind configuration and add Muuri examples in Vue addons
    - Updated Tailwind configuration to exclude example files from core and addons directories.
    - Expanded tsconfig.json to include composables directory for TypeScript support.
    - Added new composable examples for Muuri integration, including basic and events examples with corresponding styles.
    - Structured author information in package.json for the Murri composable package.

--- packages/themes/src/default/tailwind.config.ts
+++ packages/themes/src/default/tailwind.config.ts
@@ -5,5 +5,7 @@ export default {
     './crafts/**/*.{ts}',
     '../../../vue/core/src/**/*.{vue,ts,tsx}',
     '../../../vue/addons/**/*.{vue,ts,tsx}',
+    '!../../../vue/core/src/**/examples/**/*.{vue,ts,tsx}',
+    '!../../../vue/addons/**/examples/**/*.{vue,ts,tsx}',
   ],
 } satisfies Config
--- packages/themes/src/razer/tailwind.config.ts
+++ packages/themes/src/razer/tailwind.config.ts
@@ -6,5 +6,7 @@ export default {
     '../default/**/*.{ts}',
     '../../../vue/core/src/**/*.{vue,ts,tsx}',
     '../../../vue/addons/**/*.{vue,ts,tsx}',
+    '!../../../vue/core/src/**/examples/**/*.{vue,ts,tsx}',
+    '!../../../vue/addons/**/examples/**/*.{vue,ts,tsx}',
   ],
 } satisfies Config
